44234fdc3430243729d2a5e5622a2afd835d13da
DhavalPandey05 <71693691+DhavalPandey05@users.noreply.github.com>
2020-10-23 12:34:49 +0530

    Dice game

---
+++ 4-DOM-Pig-Game/app.js
@@ -0,0 +1,51 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+var scores,roundscore,activeplayer;   
+scores=[0.0];
+roundscore=0;
+activeplayer=0;
+
+
+ 
+document.querySelector('.dice').style.display='none';
+document.getElementById('score-0').textContent='0';
+document.getElementById('score-1').textContent='0';
+document.getElementById('current-0').textContent='0';
+document.getElementById('current-1').textContent='0';
+
+
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    //1 random number
+    var dice=Math.floor(Math.random()*6)+1;
+   //display result
+    var diceDom= document.querySelector('.dice');
+    diceDom.style.display='block';
+    diceDom.src= 'dice-'+dice+'.png';
+    //3.update score
+    if(dice!==1){
+        roundscore+=dice;
+        document.querySelector('#current-'+activeplayer).textContent=roundscore;
+        
+} else{
+    activeplayer===0 ? activeplayer=1 : activeplayer=0;
+    roundscore=0;
+    document.getElementById('current-0').textContent=0;
+    document.getElementById('current-0').textContent=0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('player-1-panel').classList.add('active');
+    document.querySelector('.dice').style.display='none';
+
+}
+                                                    
+});
